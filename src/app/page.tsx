c0f93f7b2cbab5ea40a2129a8ef67372a229f6b2
Alex Chaparro <AlexeChaparro@gmail.com>
2023-06-09 13:22:26 -0400

    added logic for adding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,61 @@
 "use client"; // This is a client component 👈🏽
 import Image from 'next/image'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './styles/toDo.scss';
 
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
 export default function Home() {
-  const [todos, setTodos] = useState<Array<string>>([]);
+  const [todos, setTodos] = useState<Array<Todo>>([]);
   const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
-  const handleAddTodo = () => {
+  const addTodo = () => {
     if (inputValue.trim() !== '') {
-      setTodos([...todos, inputValue]);
+       const newTodo: Todo = {
+        id: Date.now(),
+        text: inputValue,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
       setInputValue('');
     }
   };
 
-  const handleDeleteTodo = (index: number) => {
+  const deleteTodo = (index: number) => {
     const updatedTodos = todos.filter((_, i) => i !== index);
     setTodos(updatedTodos);
   };
 
+  const completeToDo = (id: number) => {
+    setTodos((prevTodos) => {
+      const updatedTodos = prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      });
+      return updatedTodos;
+    });
+  }
+
+  // addToDo
+  // deleteToDo
+  // completeToDo
+
+  useEffect(() => {
+    console.log('todos', todos)
+  }, [todos]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -36,18 +69,27 @@ export default function Home() {
             onChange={handleInputChange}
             placeholder="Enter a task..."
           />
-          <button onClick={handleAddTodo}>Add</button>
+          <button onClick={addTodo}>Add</button>
         </div>
         <ul className='w-96'>
-          {todos.map((todo, index) => (
-            <li className='mt-4 u-flex u-justifyBetween' key={index}>
-              {todo}
-              <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          <li className='mt-4 u-flex u-justifyBetween'>
+            Things to do
+          </li>
+          {todos && todos.map((todo, index) => (
+            <li 
+              className='mt-4 u-flex u-justifyBetween'
+              key={todo.id}>
+              <p className={todo.completed ? 'toDo--completed' : ''}>{todo.text}</p>
+              <div className='u-flex'>
+                <button className='mr-4' onClick={() => completeToDo(todo.id)}>Done</button>
+                <button onClick={() => deleteTodo(index)}>Delete</button>
+              </div>
             </li>
           ))}
         </ul>
       </div>
-      );
+     
+    
       
 
     </main>
